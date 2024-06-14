1828719c141a48a232b1132f55d9691a28ce0b00
hulkgosmash <hulkgosmash@proton.me>
2024-06-15 09:19:44 +1000

    Test IP Address restriction2

--- main.tf
+++ main.tf
@@ -47,31 +47,31 @@ resource "aws_security_group" "ExfilTracer_SG" {
         from_port = 25
         to_port = 25
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["${var.IP_Address}"]
     }
     ingress {
         from_port = 53
         to_port = 53
         protocol = "udp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["${var.IP_Address}"]
     }
     ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["${var.IP_Address}"]
     }
     ingress {
         from_port = 445
         to_port = 445
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["${var.IP_Address}"]
     }
     ingress {
         from_port = 3389
         to_port = 3389
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["${var.IP_Address}"]
     }
     egress {
         from_port = 0
